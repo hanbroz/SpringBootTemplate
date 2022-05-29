69daf1cd0bc076d060592e3b5bb1a154868a007c
root <hanbroz@naver.com>
2022-05-29 14:19:27 +0900

    Initial commit

---
+++ src/test/java/com/hanbroz/boot/template/SpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanbroz.boot.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
